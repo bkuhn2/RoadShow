823ef5ec2e7bab0a6216500c0e2b64024617a057
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-26 12:35:54 -0600

    Installs redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
