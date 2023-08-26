1171527adfeeef99fdef3a4be35d8ac29cc8745f
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-26 12:44:40 -0600

    Installs remaining Redux dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
 import './index.css'
+
+const store = configureStore({
+  reducer: {
+    
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>
 
