216bbe0c31bffe6fc3147c186cec86b60a3006c2
Brett Kuhn <brettkuhn87@gmail.com>
2023-08-26 12:36:32 -0600

    -

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
